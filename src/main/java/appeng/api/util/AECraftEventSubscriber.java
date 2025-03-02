6a73b88f8bd198533f68e3a1ece3d2f9cb845748
ZeroAsh <admin@remiliascarlet.com>
2025-03-02 12:07:03 +0800

    remove all github pipelies configuration files

--- src/main/java/appeng/api/util/AECraftEventSubscriber.java
+++ src/main/java/appeng/api/util/AECraftEventSubscriber.java
@@ -2,6 +2,7 @@ package appeng.api.util;
 
 import appeng.api.networking.crafting.CraftingJobStatus;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiConsumer;
 
@@ -14,7 +15,7 @@ public class AECraftEventSubscriber {
     public interface Handler {
         void handle(CraftingJobStatus job, Status status);
     }
-    private static Map<Object, BiConsumer<CraftingJobStatus, Status>> _consumers;
+    private static final Map<Object, BiConsumer<CraftingJobStatus, Status>> _consumers = new HashMap<>();
 
     public static <T extends Handler> void register(T handlerClass) {
         _consumers.remove(handlerClass);
@@ -23,8 +24,12 @@ public class AECraftEventSubscriber {
     }
 
     public static void notify(CraftingJobStatus job, Status status) {
-        for (BiConsumer<CraftingJobStatus, Status> value : _consumers.values()) {
-            value.accept(job, status);
+        try {
+            for (BiConsumer<CraftingJobStatus, Status> value : _consumers.values()) {
+                value.accept(job, status);
+            }
+        } catch (Exception e) {
+            // ignored
         }
     }
 }
